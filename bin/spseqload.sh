78125f9b1ac91bfb2530107c3c4ddff6f98abcc5
Sharon Giannatto <Sharon.Giannatto@jax.org>
2004-04-26 18:21:55 +0000

    added QC reports, use DLAFunctions, uses three config files

--- bin/spseqload.sh
+++ bin/spseqload.sh
@@ -6,11 +6,11 @@
 #  spseqload.sh
 ###########################################################################
 #
-#  Purpose:  This script controls the execution of the SwissProt Sequence Load
+#  Purpose:  This script controls the execution of the SwissProt
+#		and TrEMBL Sequence Loads
 #
-#  Usage:
-#
-#      spseqload.sh
+   Usage=spseqload.sh config_file
+#      e.g. spseqload.sh trseqload.config
 #
 #  Env Vars:
 #
@@ -19,8 +19,10 @@
 #  Inputs:
 #
 #      - Common configuration file (/usr/local/mgi/etc/common.config.sh)
-#      - SwissProt load configuration file (spseqload.config)
-#      - One or more SwissProt input files 
+#      - SwissProt/TrEMBL common configuration file (sp_common.config)
+#      - SwissProt or TrEMBL configuration file (spseqload.config or
+#             trseqload.config)
+#      - One or more  SwissProt/TrEMBL input files 
 #
 #  Outputs:
 #
@@ -33,6 +35,8 @@
 #      - Exceptions written to standard error
 #      - Configuration and initialization errors are written to a log file
 #        for the shell script
+#      - QC reports as defined by ${SEQLOAD_QCRPT} and ${MSP_QCRPT}
+#        
 #
 #  Exit Codes:
 #
@@ -59,9 +63,9 @@ rm -f ${LOG}
 #
 #  Verify the argument(s) to the shell script.
 #
-if [ $# -ne 0 ]
+if [ $# -ne 1 ]
 then
-    echo "Usage: $0" | tee -a ${LOG}
+    echo ${Usage} | tee -a ${LOG}
     exit 1
 fi
 
@@ -69,8 +73,10 @@ fi
 #  Establish the configuration file names.
 #
 CONFIG_COMMON=`pwd`/common.config.sh
-CONFIG_SPLOAD=`pwd`/spseqload.config
-echo ${CONFIG_SPLOAD}
+CONFIG_LOAD=`pwd`/$1
+CONFIG_SPCOMMON=`pwd`/sp_common.config
+
+echo ${CONFIG_LOAD}
 
 #
 #  Make sure the configuration files are readable.
@@ -80,18 +86,26 @@ then
     echo "Cannot read configuration file: ${CONFIG_COMMON}" | tee -a ${LOG}
     exit 1
 fi
-if [ ! -r ${CONFIG_SPLOAD} ]
+
+if [ ! -r ${CONFIG_LOAD} ]
 then
-    echo "Cannot read configuration file: ${CONFIG_SPLOAD}" | tee -a ${LOG}
+    echo "Cannot read configuration file: ${CONFIG_LOAD}" | tee -a ${LOG}
+    exit 1
+fi
+
+if [ ! -r ${CONFIG_SPCOMMON} ]
+then
+    echo "Cannot read configuration file: ${CONFIG_SPCOMMON}" | tee -a ${LOG}
     exit 1
 fi
 
 #
 #  Concatenate the configuration files together to produce one configuration
-#  file that can be run to set up the environment.
+#  file that can be run to set up the environment. CONFIG_SPCOMMON is depend
+#  ent on CONFIG_LOAD
 #
 CONFIG_RUNTIME=`pwd`/runtime.config
-cat ${CONFIG_COMMON} ${CONFIG_SPLOAD} > ${CONFIG_RUNTIME}
+cat ${CONFIG_COMMON} ${CONFIG_LOAD} ${CONFIG_SPCOMMON} > ${CONFIG_RUNTIME}
 . ${CONFIG_RUNTIME}
 
 echo "javaruntime:${JAVARUNTIMEOPTS}"
@@ -111,101 +125,104 @@ echo "database:${MGD_DBNAME}"
 shutDown ()
 {
     #
-    #  End the job stream if a new job key was successfully obtained.
-    #  The STAT variable will contain the return status from the data
-    #  provider loader or the clone loader.
+    # call DLA library function
     #
-    if [ ${JOBKEY} -gt 0 ]
-    then
-        echo "End the job stream" >> ${LOG_PROC}
-        ${JOBEND_CSH} ${RADAR_DBSCHEMADIR} ${JOBKEY} ${STAT}
-    fi
-    #
-    #  End the log files.
-    #
-    stopLog ${LOG_PROC} ${LOG_DIAG} ${LOG_CUR} ${LOG_VAL} | tee -a ${LOG}
+    postload
 
     #
     #  Mail the logs to the support staff.
     #
     if [ "${MAIL_LOG_PROC}" != "" ]
     then
-        mailLog ${MAIL_LOG_PROC} "SwissProt Load - Process Summary Log" ${LOG_PROC} | tee -a ${LOG}
+        mailLog ${MAIL_LOG_PROC} "SwissProt Load - Process Summary Log" \
+	    ${LOG_PROC} | tee -a ${LOG}
     fi
 
     if [ "${MAIL_LOG_CUR}" != "" ]
     then
-        mailLog ${MAIL_LOG_CUR} "SwissProt Load - Curator Summary Log" ${LOG_CUR} | tee -a ${LOG}
+        mailLog ${MAIL_LOG_CUR} "SwissProt Load - Curator Summary Log" \
+	    ${LOG_CUR} | tee -a ${LOG}
     fi
 }
 
+#
+# Function that runs to java load
+#
+
+run ()
+{
+    #
+    # log time and input files to process
+    #
+    echo "\n`date`" >> ${LOG_PROC}
+    echo "Files from stdin: ${CAT_METHOD} ${PIPED_INFILES}" | tee -a ${LOG_DIAG} 
+    #
+    # run spseqload
+    #
+    ${CAT_METHOD}  ${PIPED_INFILES}  | \
+	${JAVA_RUN} ${JAVARUNTIMEOPTS} -classpath ${CLASSPATH} \
+	-DCONFIG=${CONFIG_RUNTIME} -DJOBKEY=${JOBKEY} \
+	${SPSEQLOAD_APP} | tee -a  ${LOGDIR}/stdouterr
+
+    STAT=$?
+    if [ ${STAT} -ne 0 ]
+    then
+	echo "spseqload processing failed.  \
+	    Return status: ${STAT}" >> ${LOG_PROC}
+	shutDown
+	exit 1
+    fi
+}
+
+##################################################################
+# main
+##################################################################
 
 #
-#  Archive the log and report files from the previous run.
+# createArchive, startLog, getConfigEnv, get job key
 #
-createArchive ${ARCHIVEDIR} ${LOGDIR} ${RPTDIR} | tee -a ${LOG}
+preload
 
 #
-#  Initialize the log files.
+# need to partition if these tables are empty
 #
-startLog ${LOG_PROC} ${LOG_DIAG} ${LOG_CUR} ${LOG_VAL} | tee -a ${LOG}
-
-#
-#  Write the configuration information to the log files.
-#
-getConfigEnv >> ${LOG_PROC}
-getConfigEnv -e >> ${LOG_DIAG}
-echo "Files from stdin: ${CAT_METHOD} ${PIPED_INFILES}" >> ${LOG_DIAG}
-#
-#  Start a new job stream and get the job stream key.
-#
-echo "Start a new job stream" >> ${LOG_PROC}
-JOBKEY=`${JOBSTART_CSH} ${RADAR_DBSCHEMADIR} ${JOBSTREAM}`
-if [ $? -ne 0 ]
-then
-    echo "Could not start a new job stream for this load" >> ${LOG_PROC}
-    shutDown
-    exit 1
-fi
-echo "JOBKEY=${JOBKEY}" >> ${LOG_PROC}
-
-#
-#  Run spseqload
-#
-echo "\n`date`" >> ${LOG_PROC}
-echo "${CAT_METHOD} ${PIPED_INFILES}"
 echo 'Partitioning ACC_Accession, SEQ_Sequence, SEQ_Source_Assoc'
 ${MGD_DBSCHEMADIR}/partition/ACC_Accession_create.object
 ${MGD_DBSCHEMADIR}/partition/SEQ_Sequence_create.object
 ${MGD_DBSCHEMADIR}/partition/SEQ_Source_Assoc_create.object
 
-#gunzip -c 
-#cat 
-${CAT_METHOD} ${PIPED_INFILES} | ${JAVA_RUN} ${JAVARUNTIMEOPTS} -classpath ${CLASSPATH} -DCONFIG=${CONFIG_SPLOAD} -DJOBKEY=${JOBKEY} ${SPSEQLOAD_APP} 
-#| tee -a ${LOGDIR}/spseqloadProfile.txt
+#
+# run the load
+#
+run
 
-#-Xrunhprof:file=${LOGDIR}/spseqloadProfile.txt,format=b ${SPSEQLOAD_APP}
-
+#
+# run msp qc reports
+#
+${MSP_QCRPT} ${RADAR_DBSCHEMADIR} ${MGD_DBNAME} ${JOBKEY} ${RPTDIR}
 STAT=$?
 if [ ${STAT} -ne 0 ]
 then
-    echo "spseqload failed.  Return status: ${STAT}" >> ${LOG_PROC}
+    echo "Running MSP QC reports failed.  Return status: ${STAT}" >> ${LOG_PROC}
     shutDown
     exit 1
 fi
 
-# temporarily call this during testing to create a sequence status as deleted
-/home/lec/db/mgidbmigration/JSAM/deletedSequence.csh ${MGD_DBSCHEMADIR}
+#
+# run seqload qc reports
+#
+${SEQLOAD_QCRPT} ${RADAR_DBSCHEMADIR} ${MGD_DBNAME} ${JOBKEY} ${RPTDIR}
 STAT=$?
 if [ ${STAT} -ne 0 ]
 then
-    echo "deleteSequence.csh failed.  Return status: ${STAT}" >> ${LOG_PROC}
+    echo "Running seqloader QC reports failed.  Return status: ${STAT}" >> ${LOG_PROC}
     shutDown
     exit 1
 fi
 
-echo "spseqload completed successfully" >> ${LOG_PROC}
-
+#
+# run postload cleanup and email logs
+#
 shutDown
 
 exit 0

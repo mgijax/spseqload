3df10aacccd6137aaa8cd1ad9d4d3d6d5df4bf84
Sharon Giannatto <Sharon.Giannatto@jax.org>
2008-07-23 17:20:55 +0000

    gbseqload-4-1-1-1

--- java/org/jax/mgi/app/spseqloader/SPSeqloader.java
+++ java/org/jax/mgi/app/spseqloader/SPSeqloader.java
@@ -1,6 +1,3 @@
-//  $Header$
-//  $Name$
-
 package org.jax.mgi.app.spseqloader;
 
 import org.jax.mgi.shr.timing.Stopwatch;
@@ -73,26 +70,3 @@ public class SPSeqloader extends SeqLoader {
 
    }
 }
-// $Log
- /**************************************************************************
- *
- * Warranty Disclaimer and Copyright Notice
- *
- *  THE JACKSON LABORATORY MAKES NO REPRESENTATION ABOUT THE SUITABILITY OR
- *  ACCURACY OF THIS SOFTWARE OR DATA FOR ANY PURPOSE, AND MAKES NO WARRANTIES,
- *  EITHER EXPRESS OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR A
- *  PARTICULAR PURPOSE OR THAT THE USE OF THIS SOFTWARE OR DATA WILL NOT
- *  INFRINGE ANY THIRD PARTY PATENTS, COPYRIGHTS, TRADEMARKS, OR OTHER RIGHTS.
- *  THE SOFTWARE AND DATA ARE PROVIDED "AS IS".
- *
- *  This software and data are provided to enhance knowledge and encourage
- *  progress in the scientific community and are to be used only for research
- *  and educational purposes.  Any reproduction or use for commercial purpose
- *  is prohibited without the prior express written permission of The Jackson
- *  Laboratory.
- *
- * Copyright \251 1996, 1999, 2002, 2003 by The Jackson Laboratory
- *
- * All Rights Reserved
- *
- **************************************************************************/
--- java/org/jax/mgi/app/spseqloader/SPSequenceInterpreter.java
+++ java/org/jax/mgi/app/spseqloader/SPSequenceInterpreter.java
@@ -1,6 +1,3 @@
-//  $Header
-//  $Name
-
 package org.jax.mgi.app.spseqloader;
 
 import java.util.*;
@@ -10,7 +7,6 @@ import java.sql.*;
 import org.jax.mgi.shr.dla.input.SequenceInterpreter;
 import org.jax.mgi.shr.dla.input.SequenceInput;
 import org.jax.mgi.shr.dla.loader.seq.SeqloaderConstants;
-import org.jax.mgi.dbs.mgd.loads.SeqRefAssoc.SeqRefAssocPair;
 import org.jax.mgi.shr.dla.input.DateConverter;
 import org.jax.mgi.dbs.mgd.loads.Acc.AccessionRawAttributes;
 import org.jax.mgi.dbs.mgd.loads.SeqRefAssoc.RefAssocRawAttributes;
@@ -103,28 +99,3 @@ public class SPSequenceInterpreter extends EMBLFormatInterpreter {
         return seqInput;
     }
 }
-
-//  $Log
-
-/**************************************************************************
-*
-* Warranty Disclaimer and Copyright Notice
-*
-*  THE JACKSON LABORATORY MAKES NO REPRESENTATION ABOUT THE SUITABILITY OR
-*  ACCURACY OF THIS SOFTWARE OR DATA FOR ANY PURPOSE, AND MAKES NO WARRANTIES,
-*  EITHER EXPRESS OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR A
-*  PARTICULAR PURPOSE OR THAT THE USE OF THIS SOFTWARE OR DATA WILL NOT
-*  INFRINGE ANY THIRD PARTY PATENTS, COPYRIGHTS, TRADEMARKS, OR OTHER RIGHTS.
-*  THE SOFTWARE AND DATA ARE PROVIDED "AS IS".
-*
-*  This software and data are provided to enhance knowledge and encourage
-*  progress in the scientific community and are to be used only for research
-*  and educational purposes.  Any reproduction or use for commercial purpose
-*  is prohibited without the prior express written permission of The Jackson
-*  Laboratory.
-*
-* Copyright \251 1996, 1999, 2002, 2003 by The Jackson Laboratory
-*
-* All Rights Reserved
-*
-**************************************************************************/
